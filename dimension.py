4699491bdcd023ee948d1a0ed8f12f939d1698cb
Yukino0118 <91052421+Yukino0118@users.noreply.github.com>
2022-02-05 14:00:39 +0800

    Add files via upload

--- dimension.py
+++ dimension.py
@@ -1,6 +1,5 @@
 dimension = []
 import pandas as pd
-count = int(input('column numbers:'))
 while True:
     A = list()
     B = list()
@@ -8,15 +7,14 @@ while True:
     D = [Width,Space]
     if Width == 0 and Space == 0:
         break
-    df = pd.read_excel('dimension.xlsx', header = 2)
-    A.append(df.loc[Width,Space])
-    for i in range(1,5):
-        df2 = pd.read_excel('dimension.xlsx', header = (count + 6 + (i-1) * (count+4)))
-        B.append(df2.loc[Width,Space])
-    C = D + A + B
+    df = pd.read_excel('dimension.xlsx', header = 2 ,index_col = 0)
+    A = (df.loc[Width,Space].values.tolist())
+    C = D + A 
     dimension.append(C)
     D = list()
 with open('new.csv', 'w') as f:
     f.write('Width,Space,TCD,BCD,THK,SB,BB'+'\n')
     for d in dimension:
         f.write(str(d[0])+','+str(d[1])+','+str(d[2])+','+str(d[3])+','+str(d[4])+','+str(d[5])+','+str(d[6])+'\n')
+        
+    
