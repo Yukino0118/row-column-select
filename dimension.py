38c78458a11e2dced5ba85409706426e34d2591b
Yukino0118 <91052421+Yukino0118@users.noreply.github.com>
2022-02-06 23:44:05 +0800

    Add files via upload

--- dimension.py
+++ dimension.py
@@ -4,13 +4,13 @@ while True:
     A = list()
     B = list()
     try:
-        Width,Space = map(eval,input('Input dimension, input 0/0 quit:').split('/'))
+        Width,Space = input('Input dimension, input 0/0 quit:').split('/')
         D = [Width,Space]
-        if Width == 0 and Space == 0:
+        if Width == '0' and Space == '0':
             break
-        df = pd.read_excel('dimension.xlsx', header = 2 ,index_col = 0)
-        A = (df.loc[Width,Space].values.tolist())
-        C = D + A 
+        df = pd.read_csv('dimension.csv', header = 2, index_col = 0)
+        A = df.loc[Width,Space].tolist()
+        C = D + A
         dimension.append(C)
         D = list()
     except:
@@ -20,6 +20,6 @@ while True:
 with open('new.csv', 'w') as f:
     f.write('Width,Space,TCD,BCD,THK,SB,BB'+'\n')
     for d in dimension:
-        f.write(str(d[0])+','+str(d[1])+','+str(d[2])+','+str(d[3])+','+str(d[4])+','+str(d[5])+','+str(d[6])+'\n')
+        f.write(d[0]+','+d[1]+','+str(d[2])+','+str(d[3])+','+str(d[4])+','+str(d[5])+','+str(d[6])+'\n')
         
-    
+        
